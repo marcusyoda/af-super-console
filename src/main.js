1822363623e92026d8041082b53dcc22468f78d4
marcus <marcus@acelera.cloud>
2019-11-05 21:54:36 -0300

    [FIX] #missing-sample - Removido exemplo que ficou fixo no arquivo principal.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
 const SuperConsole = require('./superConsole');
-
-new SuperConsole('>> ALO ALO ALO <<').bg('black').log();
-new SuperConsole('>> ALO ALO ALO <<').bg('white').log();
-new SuperConsole('>> ALO ALO ALO <<').bg('grey').log();
-new SuperConsole('>> ALO ALO ALO <<').bg('red').log();
-new SuperConsole('>> ALO ALO ALO <<').bg('magenta').log();
-new SuperConsole('>> ALO ALO ALO <<').bg('yellow').log();
-new SuperConsole('>> ALO ALO ALO <<').bg('cyan').log();
-new SuperConsole('>> ALO ALO ALO <<').bg('green').log();
-
 module.exports = {
   SuperConsole,
 };
